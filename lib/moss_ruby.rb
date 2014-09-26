66da5bf31dd9aa41e5bb3dd654325f31c894a261
Andrew Cain <acain@swin.edu.au>
2014-09-26 22:06:30 +1000

    add ability to upload files and check results. start spec tests

--- lib/moss_ruby.rb
+++ lib/moss_ruby.rb
@@ -21,48 +21,158 @@
 # SOFTWARE.
 
 require 'socket'
+require 'open-uri'
 
 #
 #
 #
 class MossRuby
+	attr_accessor   :userid
+	attr_accessor   :server
+	attr_accessor   :port
+	attr_reader     :options
+
+	def self.empty_file_hash
+		{ base_files: Array.new, files: Array.new }
+	end
+
+	def self.add_base_file ( hash, file )
+		hash[:base_files] << file
+		hash
+	end
+
+	def self.add_file ( hash, file )
+		hash[:files] << file
+		hash
+	end
 
 	def initialize(userid, server = "moss.stanford.edu", port = 7690)
 		@options = {
-			max_times: { key: "m", value: 10 },
-			directory_submission: { key: "d", value: false },
-			show_num_matches: { key: "n", value: 250 },
-			experimental_server: { key: "x", value: false },
-			comment: { key: "c", value:"" },
-			language: { key: "l", value:"c" }
+			max_matches:            10,
+			directory_submission:   false,
+			show_num_matches:       250,
+			experimental_server:    false,
+			comment:                "",
+			language:               "c"
 		}
 		@server = server
 		@port = port
 		@userid = userid
 	end
 
+	def upload_file (moss_server, file, id = 0)
+		filename = file.strip.tap do |name|
+			name.gsub! /[^\w\-\/.]/, '_'
+		end
+
+		content = IO.read(filename)
+		size = content.length
+
+		moss_server.write "file #{id} #{@options[:language]} #{size} #{file}\n"
+		moss_server.write content
+	end
+
 	def check(files_dict)
+		# Chech that the files_dict contains valid filenames
+		files_dict[:base_files].each do |file_search|
+			if Dir[file_search].length == 0
+				raise "Unable to locate base file(s) matching #{file_search}"
+			end
+		end
+
+		if files_dict[:files].length == 0
+			return
+		end
+
+		files_dict[:files].each do |file_search|
+			if Dir[file_search].length == 0
+				raise "Unable to locate base file(s) matching #{file_search}"
+			end
+		end
+
 		# Connect to the server
 		moss_server = TCPSocket.new @server, @port
 		begin
 			# Send header details
-			moss_server.puts "moss #{@userid}\n"
-			moss_server.puts "directory #{@options[:directory_submission][:value] ? 1 : 0 }\n"
-			moss_server.puts "X #{@options[:experimental_server][:value] ? 1 : 0}\n"
-			moss_server.puts "maxmatches #{@options[:max_times][:value]}\n"
-			moss_server.puts "show #{@options[:show_num_matches][:value]}\n"
+			moss_server.write "moss #{@userid}\n"
+			moss_server.write "directory #{@options[:directory_submission] ? 1 : 0 }\n"
+			moss_server.write "X #{@options[:experimental_server] ? 1 : 0}\n"
+			moss_server.write "maxmatches #{@options[:max_matches]}\n"
+			moss_server.write "show #{@options[:show_num_matches]}\n"
 
 			# Send language option
-			moss_server.puts "language #{@options[:language][:value]}\n"
+			moss_server.write "language #{@options[:language]}\n"
 
 			line = moss_server.gets
 			if line.strip() != "yes"
+				moss_server.write "end\n"
 				raise "Invalid language option."
 			end
 
-			moss_server.puts "end\n"
+			files_dict[:base_files].each do |file_search|
+				Dir[file_search].each do |file|
+					upload_file moss_server, file
+				end
+			end
+
+			idx = 1
+			files_dict[:files].each do |file_search|
+				Dir[file_search].each do |file|
+					upload_file moss_server, file, idx
+					idx += 1
+				end
+			end
+
+			moss_server.write "query 0 #{@options[:comment]}\n"
+
+			result = moss_server.gets
+
+			moss_server.write "end\n"
+			return result
 		ensure
 			moss_server.close
 		end
+	end
+
+	def extract_results(uri)
+		result = Array.new
+		begin
+			match = 0
+			match_file = Array.new
+			data = Array.new
+			while true
+				# read the two match files
+				match_top = open("#{uri}/match#{match}-top.html").read
+				match_file[0] = open("#{uri}/match#{match}-0.html").read
+				match_file[1] = open("#{uri}/match#{match}-1.html").read
+
+				data[0] = read_data match_file[0]
+				data[1] = read_data match_file[1]
+				top = read_pcts match_top
+
+				result << { 
+					files: 	[ data[0][:filename], data[1][:filename] ], 
+					html:  	[ "<PRE>#{data[0][:html]}</PRE>", "<PRE>#{data[0][:html]}</PRE>" ],
+					pct:  	[ top[:pct0], top[:pct1] ]
+				}
+
+				match += 1
+			end
+		rescue OpenURI::HTTPError
+		end
+
+		result
+	end
+
+	private
+
+	def read_data(match_file)
+		regex = /<HR>\s+(?<filename>\S+)<p><PRE>\n(?<html>.*)<\/PRE>\n<\/PRE>\n<\/BODY>\n<\/HTML>/xm
+		match_file.match(regex)
+	end
+
+	def read_pcts(top_file)
+		regex = /<TH>(?<filename0>\S+)\s\((?<pct0>\d+)%\).*<TH>(?<filename1>\S+)\s\((?<pct1>\d+)%\)/xm
+		top_file.match(regex)
 	end
 end
---
+++ spec/moss_ruby_spec.rb
@@ -0,0 +1,138 @@
+require 'spec_helper'
+ 
+describe MossRuby do
+
+	before :each do
+    	@moss = MossRuby.new "myid"
+	end
+
+	describe "#new" do
+    	it "takes the id parameter and returns a MossRuby object" do
+        	expect(@moss).to be_an_instance_of MossRuby
+    	end
+
+    	it "creates an object with default server path" do
+    		expect(@moss.server).to eql "moss.stanford.edu" 
+    	end
+
+    	it "creates an object with default port" do
+    		expect(@moss.port).to eql 7690 
+    	end
+
+    	it "creates an object with the passed in userid" do
+    		expect(@moss.userid).to eql "myid" 
+    	end
+
+    	it "creates an object with default options" do
+    		expect(@moss.options.has_key? :max_matches).to eql true
+    		expect(@moss.options.has_key? :directory_submission).to eql true
+    		expect(@moss.options.has_key? :show_num_matches).to eql true
+    		expect(@moss.options.has_key? :experimental_server).to eql true
+    		expect(@moss.options.has_key? :comment).to eql true
+    		expect(@moss.options.has_key? :language).to eql true
+    		expect(@moss.options.length).to eql 6
+    	end
+	end
+
+	describe "#check" do
+
+		before :each do
+    		@server = double('server')
+    		allow(TCPSocket).to receive(:new).and_return(@server)
+		end
+
+		def file_hash
+			result = MossRuby.empty_file_hash
+			test_dir = File.join(File.dirname(__FILE__), 'test_files')
+			MossRuby.add_file(result, "#{test_dir}/*.c")
+			result
+		end
+
+		it "opens a TCP" do
+			expect(@server).to receive(:write).at_least(:once)
+			expect(@server).to receive(:gets) { "yes" }
+			expect(@server).to receive(:close)
+			
+			@moss.check file_hash
+		end
+
+		it "raises an exception if the language is not known" do
+			expect(@server).to receive(:write).at_least(:once)
+			expect(@server).to receive(:gets) { "no" }
+			expect(@server).to receive(:close)
+			
+			expect { @moss.check file_hash }.to raise_error("Invalid language option.")
+		end
+
+		it "sends requests to the server using the default options" do
+			expect(@server).to receive(:write).with("moss myid\n")
+			expect(@server).to receive(:write).with("directory 0\n")
+			expect(@server).to receive(:write).with("X 0\n")
+			expect(@server).to receive(:write).with("maxmatches 10\n")
+			expect(@server).to receive(:write).with("show 250\n")
+			expect(@server).to receive(:gets) { "yes" }
+			expect(@server).to receive(:write).with("language c\n")
+			expect(@server).to receive(:write).with("query 0 \n")
+			expect(@server).to receive(:write).with("end\n")
+			expect(@server).to receive(:close)
+
+			allow(@server).to receive(:write)
+
+			@moss.check file_hash
+		end
+
+		it "sends requests to the server using supplied options" do
+			@moss.userid = "fred"
+			@moss.options[:directory_submission] = true
+			@moss.options[:experimental_server] = true
+			@moss.options[:max_matches] = 100
+			@moss.options[:show_num_matches] = 85
+			@moss.options[:language] = "python"
+			@moss.options[:comment] = "Hello World"
+
+			expect(@server).to receive(:write).with("moss fred\n")
+			expect(@server).to receive(:write).with("directory 1\n")
+			expect(@server).to receive(:write).with("X 1\n")
+			expect(@server).to receive(:write).with("maxmatches 100\n")
+			expect(@server).to receive(:write).with("show 85\n")
+			expect(@server).to receive(:gets) { "yes" }
+			expect(@server).to receive(:write).with("language python\n")
+			expect(@server).to receive(:write).with("query 0 Hello World\n")
+			expect(@server).to receive(:write).with("end\n")
+			expect(@server).to receive(:close)
+
+			allow(@server).to receive(:write)
+
+			@moss.check file_hash
+		end
+
+		RSpec::Matchers.define :a_file_like do |filename, lang|
+  			match { |actual| /file [0-9]+ c [0-9]+ .*#{filename}\n/.match(actual) }
+		end
+		RSpec::Matchers.define :text_starting_with do |line|
+  			match { |actual| actual.start_with? line }
+		end
+
+		it "sends files it is provided" do
+			expect(@server).to receive(:write).with("moss myid\n")
+			expect(@server).to receive(:write).with("directory 0\n")
+			expect(@server).to receive(:write).with("X 0\n")
+			expect(@server).to receive(:write).with("maxmatches 10\n")
+			expect(@server).to receive(:write).with("show 250\n")
+			expect(@server).to receive(:gets) { "yes" }
+			expect(@server).to receive(:write).with("language c\n")
+			expect(@server).to receive(:write).with("query 0 \n")
+			expect(@server).to receive(:write).with("end\n")
+			expect(@server).to receive(:close)
+
+			expect(@server).to receive(:write).with(a_file_like("hello.c", "c"))
+			expect(@server).to receive(:write).with(text_starting_with("#include <stdio.h>\n\nint main()")).twice
+
+			expect(@server).to receive(:write).with(a_file_like("hello2.c", "c"))
+
+			@moss.check file_hash
+		end
+
+	end
+
+end
